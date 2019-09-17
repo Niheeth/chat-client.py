9eded5d3d28648f29afe6f36ee29ccda2bd72265
Niheeth Koneru <39443048+Niheeth@users.noreply.github.com>
2019-09-17 09:42:24 +0200

    Update chatclient.py
    added message receiving option.

--- chatclient.py
+++ chatclient.py
@@ -1,5 +1,5 @@
 import socket,sys,select,string
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 if (len(sys.argv) < 3):
 	print("ENTER: python chat-client.py hostname:port username")
 	sys.exit()
@@ -7,9 +7,24 @@ in_list = sys.argv[1].split(':')
 HOSTNAME = in_list[0]
 PORTNUMBER = int(in_list[1])
 username = sys.argv[2]
-s.settimeout(8)
-s.connect((HOSTNAME,PORTNUMBER))
+soc.settimeout(8)
+soc.connect((HOSTNAME,PORTNUMBER))
 print("connection established")
 nick = 'NICK '+ username
-s.send(nick)
+soc.send(nick)
 x =''
+while True :
+	list_of_socket = [sys.stdin,soc]
+	read_list,write_list,error_list = select.select(list_of_socket,[], [])
+	for i in read_list:
+		if i == soc:
+			message_recieved = i.recv(2048)
+			if not message_recieved:
+				print("connection-lost")
+				sys.exit()
+			else:
+			        if message_recieved != 'MSG' +username=' '+x :
+			        	sys.stdout.write(message_recieved.strip('MSG '))
+			        	sys.stdout.flush()
+			        else :
+			            continue
